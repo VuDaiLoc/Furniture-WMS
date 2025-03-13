c4d2107305f2c4a1fc5123d3cd6befea4a4af831
Lộc Vũ <locvu2912@gmail.com>
2025-03-13 21:25:26 +0700

    Update 13/3/2025

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Furniture_WMS.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
